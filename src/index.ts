8589b82f8dec840eb1844df25d3bc32d7a0a5942
codeinsack <codeinsack@gmail.com>
2020-04-13 22:18:06 +0300

    test string

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { NumbersCollection } from './NumbersCollection'
 import { CharactersCollection } from './CharactersCollection'
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0, 2])
-const charactersCollection = new CharactersCollection('eugene')
+const charactersCollection = new CharactersCollection('abracadabra')
 const numberSorter = new Sorter(numbersCollection)
 const stringSorter = new Sorter(charactersCollection)
 numberSorter.sort()
