743dffcef9a21097cd43e9cb132a5d9e03bc02c2
codeinsack <codeinsack@gmail.com>
2020-04-13 22:16:30 +0300

    add characters collection

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from './NumbersCollection'
+import { CharactersCollection } from './CharactersCollection'
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0])
-const sorter = new Sorter(numbersCollection)
-sorter.sort()
+const numbersCollection = new NumbersCollection([10, 3, -5, 0, 2])
+const charactersCollection = new CharactersCollection('eugene')
+const numberSorter = new Sorter(numbersCollection)
+const stringSorter = new Sorter(charactersCollection)
+numberSorter.sort()
+stringSorter.sort()
 console.log(numbersCollection.data)
+console.log(charactersCollection.data)
