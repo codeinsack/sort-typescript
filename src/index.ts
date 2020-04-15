9ca1959bddb4e59489a5cbe1b4f667e5bf376fd7
codeinsack <codeinsack@gmail.com>
2020-04-15 19:46:22 +0300

    test linked list

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from './NumbersCollection'
 import { CharactersCollection } from './CharactersCollection'
+import { LinkedList } from './LinkedList'
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0, 2])
 const charactersCollection = new CharactersCollection('abracadabra')
@@ -10,3 +11,13 @@ numberSorter.sort()
 stringSorter.sort()
 console.log(numbersCollection.data)
 console.log(charactersCollection.data)
+
+const linkedList = new LinkedList()
+linkedList.add(500)
+linkedList.add(-10)
+linkedList.add(-3)
+linkedList.add(4)
+
+const linkedSorter = new Sorter(linkedList)
+linkedSorter.sort()
+linkedList.print()
